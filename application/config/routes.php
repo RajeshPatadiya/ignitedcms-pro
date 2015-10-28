3f84a92ba695c02f5cf150d63dbb49531e45377a
ignitedcms <kungfuthemes@hotmail.com>
2015-10-28 18:38:50 +0000

    Added 404
    Added custom 404 error page

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'enter';
-$route['404_override'] = '';
+$route['404_override'] = 'My_404';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['page/(:num)/(:num)'] = 'admin/test_twig/display/$1/$2';
